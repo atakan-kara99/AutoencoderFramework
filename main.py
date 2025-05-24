60f77546e04e1e0554586faf9a035361d0dd49af
Yilmaz Atakan Kara <stu216493@mail.uni-kiel.de>
2025-05-24 11:21:43 +0200

    Refactor imports in main.py for clarity; remove unused dataset imports and update trainer model and dataset initialization.

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 from trainer import Trainer
 from visualizer import Visualizer
 
-# 2D to 1D
-from model import Ae2Dto1D, Ae2Dto1Dlinear
-from dataset import Ds2Dbut2Dsingle, Ds2Dbut1Dsingle, Ds2Dbut2Dmulti
-
-# 3D to 1D
+from model import Ae2Dto1D, Ae2Dto1Dlinear, Ae2Dto1Dwide
 from model import Ae3Dto1D
-from dataset import Ds3Dbut3Dsingle, Ds3Dbut2Dsingle, Ds3Dbut1Dsingle, Ds3Dbut2Dmulti
-
-# 3D to 2D
 from model import Ae3Dto2D, Ae3Dto2Dlinear, Ae3Dto2Dwide
+from model import StandardVAE, MixtureVAE
+
+from dataset import Ds2Dbut2Dsingle, Ds2Dbut1Dsingle, Ds2Dbut2Dmulti
+from dataset import Ds3Dbut3Dsingle, Ds3Dbut2Dsingle, Ds3Dbut1Dsingle, Ds3Dbut2Dmulti
 from dataset import Ds3Dbut3Dsingle, Ds3Dbut2Dsingle, Ds3Dbut1Dsingle, Ds3Dbut2Dmulti, Ds3Dbut3Dmulti
-
-# Moons
 from dataset import Ds2DMoons, Ds3DMoons, DsTrue3DMoons
-
-# Swiss Roll
-from dataset import Ds3DSwissRoll
+from dataset import Ds2DSwissRoll, Ds3DSwissRoll
+from dataset import Ds3DTorus
+from dataset import Ds3DSphere
 
 
 trainer = Trainer(
-    model=Ae3Dto2Dwide(),
-    dataset=Ds3DSwissRoll(),
-    loss_names=['mse'],
+    model=StandardVAE(3, 64, 2),
+    dataset=Ds3DTorus(),
+    losses={'vae': 1.0},
     sample_neighbors=True,
     batch_size=8,
     ).train()
