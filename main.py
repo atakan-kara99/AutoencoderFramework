552ae46246d4cd76332c0bbc192f67ee936a5cdb
Yilmaz Atakan Kara <stu216493@mail.uni-kiel.de>
2025-08-20 12:37:14 +0200

    Refactor Trainer initialization: update model parameters to use dynamic hidden layer sizes and load dataset from file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dataset import load
 from trainer import Trainer
 from visualizer import Visualizer
 
@@ -5,16 +6,24 @@ from model import AE, VAE
 
 from dataset import (
     Ds2Dbut2Dsingle, Ds2Dbut1Dsingle, Ds2Dbut2Dmulti,
-    Ds3Dbut3Dsingle, Ds3Dbut2Dsingle, Ds3Dbut1Dsingle, Ds3Dbut2Dmulti, Ds3Dbut3Dmulti,
+    Ds3Dbut3Dsingle, Ds3Dbut2Dsingle, Ds3Dbut1Dsingle, Ds3Dbut2Dmulti, Ds3Dbut2Dsingle,
     Ds2DMoons, Ds3DMoons, DsTrue3DMoons,
     Ds2DSwissRoll, Ds3DSwissRoll,
     Ds3DTorus,
     Ds3DSphere,
 )
 
+# 2D
+# (2, [32, 16, 8], 1)
+# Ds2Dbut2Dsingle, Ds2Dbut2Dmulti, Ds2DMoons, Ds2DSwissRoll
+
+# 3D
+# (3, [64, 32, 16], 2)
+# Ds3Dbut2Dsingle, Ds3Dbut2Dmulti, Ds3DMoons, Ds3DSwissRoll, Ds3DTorus, Ds3DSphere
+
 trainer = Trainer(
-    model=AE(3, 64, 2),
-    dataset=Ds3DTorus(),
+    model=AE(3, [64, 32, 16], 2),
+    dataset=load("Ds3DSwissRoll.pt"),
     losses={'tri': 1.0},
     sample_neighbors=False,
     batch_size=32,
